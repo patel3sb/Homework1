8b9ff0888adfc77defbecc87b0f9d3843a70f8ec
patel3sb <patel3sb@mail.uc.edu>
2024-05-10 23:01:06 -0400

    formatted the file apps.py

--- Homework1/app.py
+++ Homework1/app.py
@@ -1,18 +1,20 @@
 import os
-from flask import Flask , redirect , render_template, request
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager , UserMixin , login_required ,login_user, logout_user,current_user
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.db'
-app.config['SECRET_KEY']='619619'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SECRET_KEY'] = '619619'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-class User(UserMixin,db.Model):
-    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(200))
     last_name = db.Column(db.String(200))
     username = db.Column(db.String(200))
@@ -23,27 +25,28 @@ class User(UserMixin,db.Model):
         return f'<User: {self.username}>'
 
 
-
 @login_manager.user_loader
 def get(id):
     return User.query.get(id)
 
-@app.route('/',methods=['GET'])
+
+@app.route('/', methods=['GET'])
 @login_required
 def get_home():
     return render_template('home.html')
 
-@app.route('/login',methods=['GET'])
+
+@app.route('/login', methods=['GET'])
 def get_login():
     return render_template('login.html')
 
 
-@app.route('/signup',methods=['GET'])
+@app.route('/signup', methods=['GET'])
 def get_signup():
     return render_template('signup.html')
 
 
-@app.route('/login',methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login_post():
     username = request.form['username']
     password = request.form['password']
@@ -51,31 +54,33 @@ def login_post():
     login_user(user)
     return redirect('/')
 
-@app.route('/signup',methods=['POST'])
+
+@app.route('/signup', methods=['POST'])
 def signup_post():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
-    user = User(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+    user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
     db.session.add(user)
     db.session.commit()
     user = User.query.filter_by(username=username).first()
     login_user(user)
     return redirect('/')
- 
+
+
 @app.route('/users')
 def users():
     users = User.query.all()
     return render_template('users.html', users=users)
 
 
-@app.route('/logout',methods=['GET'])
+@app.route('/logout', methods=['GET'])
 def logout():
     logout_user()
     return redirect('/login')
 
-if __name__=='__main__':
-    app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(debug=True)
